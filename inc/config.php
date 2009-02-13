342356fe45e2d0638a02a5148c11d11ebd7176df
zach <zach@krutimedia.com>
2009-02-13 11:50:33 -0500

    added basic config vars

---
+++ inc/config.php
@@ -0,0 +1,13 @@
+<?php # (C) 2009 KrutiMedia Productions, Inc
+
+$conf_site_name = "";
+$conf_site_url = "";
+$conf_site_meta_desc = "";
+$conf_site_meta_keys = "";
+$conf_site_status = "";
+$conf_db_host = "";
+$conf_db_user = "";
+$conf_db_pass = "";
+$conf_db_name = "";
+
+?>
