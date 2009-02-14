a4752c3040e987d872a4feaafd01c7c6e35bf69b
zach <zach@krutimedia.com>
2009-02-14 02:52:34 -0500

    update

---
+++ inc/header.php
@@ -0,0 +1,23 @@
+<?php # (C) 2009 KrutiMedia Productions, Inc
+      # Trimba CMS - core includes: header
+
+ob_start();
+
+session_start();
+
+if (!isset($page_title)) {
+	$page_title = '$conf_site_name';
+}
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en">
+<head>
+<meta name="description" content="<?php echo ' . $conf_site_meta_desc . ' ?>" />
+<meta name="keywords" content="<?php echo ' . $conf_site_meta_keywords . ' ?>" />
+
+<?php
+	include('style_data.php');
+	include('script_data.php');
+?>
+</head>
