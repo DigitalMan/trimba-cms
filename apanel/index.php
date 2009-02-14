7c593741c9773cf7e38f4d12d9fe99eb9829aeb3
zach <zach@krutimedia.com>
2009-02-14 02:11:07 -0500

    add login box

---
+++ apanel/index.php
@@ -0,0 +1,14 @@
+<body>
+<table width="428" height="235" border="2" align="center" bordercolor="#666666">
+  <tr>
+    <td width="428" align="center" valign="middle"><div align="center">
+      <p>Truvia Content Management System</p>
+      <p>Please authenticate yourself below to being your session.</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+    </div></td>
+  </tr>
+</table>
+</body>
