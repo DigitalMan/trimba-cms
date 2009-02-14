73e659d264fbe2d41ef0a42d5a735e09d3dbc9f6
zach <zach@krutimedia.com>
2009-02-14 02:27:31 -0500

    doesnt do anything, but looks pretty

--- apanel/index.php
+++ apanel/index.php
@@ -1,14 +1,7 @@
 <body>
 <table width="428" height="235" border="2" align="center" bordercolor="#666666">
   <tr>
-    <td width="428" align="center" valign="middle"><div align="center">
-      <p>Truvia Content Management System</p>
-      <p>Please authenticate yourself below to being your session.</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-    </div></td>
+    <td width="428" align="center" valign="middle" nowrap="nowrap" bordercolor="#F0F0F0"><div align="center"><img src="media/images/logo.png" /></div></td>
   </tr>
 </table>
 </body>
